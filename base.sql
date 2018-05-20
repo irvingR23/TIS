2abbda4d4603d8009e6b2582c22b5ce14e0a4aa2
irving <sefiro2016@gmail.com>
2018-05-20 15:51:00 -0500

    Agregado prestamo

---
+++ base.sql
@@ -0,0 +1,171 @@
+-- MySQL dump 10.16  Distrib 10.1.31-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: banco
+-- ------------------------------------------------------
+-- Server version	5.6.40-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `administrador`
+--
+
+DROP TABLE IF EXISTS `administrador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `administrador` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(15) DEFAULT NULL,
+  `contrasenia` varchar(25) DEFAULT NULL,
+  `nombre` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `administrador`
+--
+
+LOCK TABLES `administrador` WRITE;
+/*!40000 ALTER TABLE `administrador` DISABLE KEYS */;
+INSERT INTO `administrador` VALUES (1,'juan','1234','juani'),(2,'ash','12','steve');
+/*!40000 ALTER TABLE `administrador` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cliente` (
+  `numeroCuenta` int(11) NOT NULL AUTO_INCREMENT,
+  `contrasenia` varchar(25) DEFAULT NULL,
+  `usuario` varchar(15) DEFAULT NULL,
+  `nombre` varchar(20) DEFAULT NULL,
+  `correo` varchar(30) DEFAULT NULL,
+  `RFC` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`numeroCuenta`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (5,'123','pika','pikachu',NULL,NULL);
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `peticion`
+--
+
+DROP TABLE IF EXISTS `peticion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `peticion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(20) DEFAULT NULL,
+  `usuario` int(11) DEFAULT NULL,
+  `admin` int(11) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `estado` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuario` (`usuario`),
+  KEY `admin` (`admin`),
+  CONSTRAINT `peticion_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `cliente` (`numeroCuenta`),
+  CONSTRAINT `peticion_ibfk_2` FOREIGN KEY (`admin`) REFERENCES `administrador` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `peticion`
+--
+
+LOCK TABLES `peticion` WRITE;
+/*!40000 ALTER TABLE `peticion` DISABLE KEYS */;
+INSERT INTO `peticion` VALUES (1,'peticion',5,1,120,'pendiente');
+/*!40000 ALTER TABLE `peticion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tarjeta`
+--
+
+DROP TABLE IF EXISTS `tarjeta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tarjeta` (
+  `numero` int(11) NOT NULL AUTO_INCREMENT,
+  `credDisp` int(11) DEFAULT NULL,
+  `deuda` int(11) DEFAULT NULL,
+  `saldo` int(11) DEFAULT NULL,
+  `usuario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`numero`),
+  KEY `usuario` (`usuario`),
+  CONSTRAINT `tarjeta_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `cliente` (`numeroCuenta`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tarjeta`
+--
+
+LOCK TABLES `tarjeta` WRITE;
+/*!40000 ALTER TABLE `tarjeta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tarjeta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transac`
+--
+
+DROP TABLE IF EXISTS `transac`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transac` (
+  `numero` int(11) NOT NULL AUTO_INCREMENT,
+  `cantidad` int(11) DEFAULT NULL,
+  `cuDest` int(11) DEFAULT NULL,
+  `cuRe` int(11) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  PRIMARY KEY (`numero`),
+  KEY `cuRe` (`cuRe`),
+  KEY `cuDest` (`cuDest`),
+  CONSTRAINT `transac_ibfk_1` FOREIGN KEY (`cuRe`) REFERENCES `cliente` (`numeroCuenta`),
+  CONSTRAINT `transac_ibfk_2` FOREIGN KEY (`cuDest`) REFERENCES `cliente` (`numeroCuenta`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transac`
+--
+
+LOCK TABLES `transac` WRITE;
+/*!40000 ALTER TABLE `transac` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transac` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-05-20 15:45:40
--- main.py
+++ main.py
@@ -1,11 +1,44 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for,session
 import MySQLdb
 app= Flask(__name__)
 
+app.secret_key = 'secreto'
+
 conn=MySQLdb.connect(host="localhost",user="root",password="1234",db="banco")
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def main():
+	session['username']=' '
+	if request.method=='POST':
+		user = str(request.form["usuario"])
+		ps = str(request.form["contrasenia"])
+		cursor =conn.cursor()
+		try:
+			cursor.execute("select * from cliente where usuario =%s",(user,))
+			us= cursor.fetchone()
+			if us[2] == user:
+				if us[1] == ps:
+					session['logged_in']=True
+					session.pop('username',None)
+					session['username']=us[2]
+					return redirect(url_for('perfil'))
+				else:
+					return "fallo al iniciar sesión"
+		except:
+			try:
+				cursor.execute("select * from administrador where usuario =%s",(user,))
+				us= cursor.fetchone()
+				if us[1] == user:
+					if us[2] == ps:
+						session['logged_in']=True
+						session.pop('username',None)
+						session['username']=us[1]
+						return redirect(url_for('perfila'))
+					else:
+						return "fallo al iniciar sesión"
+			except:
+				return 'error'
 	return render_template('Index2.html')
+	
 
 @app.route("/registro",methods=['GET','POST'])
 def registro():
@@ -22,24 +55,41 @@ def registro():
 
 @app.route("/perfil")
 def perfil():
-	return render_template("perfil.html")
+	cursor=conn.cursor()
+	usern=session.get('username')
+	cursor.execute("select * from cliente where usuario=%s",(usern,))
+	datos=cursor.fetchone()
+	return render_template("perfil.html",datos=datos)
 
-@app.route("/comprobar",methods=['POST'])
-def comprobar():
-	if request.method=='POST':
-		user = str(request.form["usuario"])
-		ps = str(request.form["contrasenia"])
-		cursor =conn.cursor()
-		try:
-			cursor.execute("select * from cliente where usuario =%s",(user,))
-			us= cursor.fetchone()
-			if us[2] == user:
-				if us[1] == ps:
-					return redirect(url_for('perfil'))
-				else:
-					return "fallo al iniciar sesión"
-		except:
-			return 'error'
+@app.route("/salir")
+def salir():
+	session['logged_in']=False
+	return redirect(url_for("main"))
+
+@app.route("/homes")
+def homes():
+	return redirect(url_for("main"))	
+
+@app.route("/prestamo",methods=['POST','GET'])
+def prestamo():
+	if request.method=='POST' and session["logged_in"]==True:
+		can=int(request.form["cantidad"])
+		cursor = conn.cursor()
+		noUs=session.get('username')
+		cursor.execute("select numeroCuenta from cliente where usuario=%s",(noUs,))
+		us=cursor.fetchone()
+		cursor.execute("INSERT INTO peticion(nombre,usuario,admin,cantidad,estado) values('peticion',%s,1,%s,'pendiente')",(us[0],can))
+		conn.commit()
+		cursor.close()
+	return render_template('SPrestamo.html')
+
+@app.route("/perfila")
+def  perfila():
+	cursor=conn.cursor()
+	usern=session.get('username')
+	cursor.execute("select * from administrador where usuario=%s",(usern,))
+	datos=cursor.fetchone()
+	return render_template("PerfilAdminist.html",datos=datos)
 
 if __name__ == "__main__":
 	app.run(debug=True)
